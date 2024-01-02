72f2e3d96ffa562d9f4317167de20f4421837f52
zincah <leeah9737@naver.com>
2024-01-02 21:18:34 +0900

    first commit2

---
+++ src/main/java/com/ysl/calendar/YslCalendarWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ysl.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YslCalendarWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YslCalendarWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ysl/calendar/YslCalendarWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysl.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YslCalendarWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
