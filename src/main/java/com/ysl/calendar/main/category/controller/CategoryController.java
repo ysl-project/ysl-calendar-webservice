f152f3374cb79cb7bb4f182d323acbcac3051b8d
zincah <leeah9737@naver.com>
2024-07-25 22:35:54 +0900

    fix : category 추가 및 전체 리스트 조회 로직

--- src/main/java/com/ysl/calendar/main/category/controller/CategoryController.java
+++ src/main/java/com/ysl/calendar/main/category/controller/CategoryController.java
@@ -4,10 +4,7 @@ import com.ysl.calendar.domain.categories.CalendarCategory;
 import com.ysl.calendar.main.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
@@ -20,33 +17,20 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    /* 카테고리 추가 */
     @PostMapping
     public ArrayList<CalendarCategory> addCategory(@ModelAttribute CalendarCategory calendarCategory, ArrayList<CalendarCategory> categoryList){
-        log.debug("== category insert start ==");
-        log.debug("전달된 카테고리 정보 [" + calendarCategory.toString() + "]");
-        String createUser = calendarCategory.getCreate_user();
-
-        try{
-            if(!categoryService.save(calendarCategory)){
-                throw new Exception();
-            }
-
-            log.debug("카테고리 저장 완료. 전체 리스트를 조회합니다.");
-
-            ArrayList<Object> objList = categoryService.selectAll(createUser);
-            categoryList = new ArrayList<>();
-            for(Object obj : objList){
-                if(obj instanceof CalendarCategory){
-                    categoryList.add((CalendarCategory) obj);
-                }
-            }
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
+        categoryList = categoryService.addCategory(calendarCategory);
         return categoryList;
     }
+
+    /* 유저별 카테고리 조회 */
+    @GetMapping
+    public ArrayList<CalendarCategory> selectCategoryAll(@RequestParam("id") String userId, ArrayList<CalendarCategory> categoryList){
+        return categoryService.selectCategoryAll(userId);
+    }
+
+    /* 카테고리 수정 */
 
 
 }
--- src/main/java/com/ysl/calendar/main/category/service/CategoryService.java
+++ src/main/java/com/ysl/calendar/main/category/service/CategoryService.java
@@ -15,12 +15,55 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
-    public boolean save(CalendarCategory category){
-        return categoryRepository.save(category);
+    public ArrayList<CalendarCategory> addCategory(CalendarCategory calendarCategory){
+
+        log.debug("== 카테고리 insert ==");
+        log.debug("전달된 카테고리 정보 [" + calendarCategory.toString() + "]");
+        String createUser = calendarCategory.getCreate_user();
+        ArrayList<CalendarCategory> returnList = null;
+
+        try{
+            if(!categoryRepository.save(calendarCategory)){
+                // 중복일 경우 또는 db insert 가 안될 경우
+                throw new Exception();
+            }
+
+            log.debug("카테고리 저장 완료. 전체 카테고리 리스트를 조회 start");
+
+            ArrayList<Object> objList = categoryRepository.selectAll(createUser);
+            returnList = new ArrayList<>();
+            for(Object obj : objList){
+                if(obj instanceof CalendarCategory){
+                    returnList.add((CalendarCategory) obj);
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        log.debug("returnList [" + returnList.toString() + "]");
+        return returnList;
     }
 
-    public ArrayList<Object> selectAll(String createUser){
-        return categoryRepository.selectAll(createUser);
+    public ArrayList<CalendarCategory> selectCategoryAll(String createUser){
+        log.debug("== 전체 카테고리 리스트 조회 start ==");
+        ArrayList<CalendarCategory> returnList = null;
+
+        try{
+            ArrayList<Object> objList = categoryRepository.selectAll(createUser);
+            returnList = new ArrayList<>();
+            for(Object obj : objList){
+                if(obj instanceof CalendarCategory){
+                    returnList.add((CalendarCategory) obj);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        log.debug("returnList [" + returnList.toString() + "]");
+        return returnList;
     }
 
 
